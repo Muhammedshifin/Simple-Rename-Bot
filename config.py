0df274ffa2eb12199bac5da3eb75c6f0a9c7d1b1
Muhammedshifin <muhammedshifink4@gmail.com>
2023-04-03 19:54:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ API_HASH = environ.get("API_HASH", "03c2cced4dea9b1e96dce87558dd2381")
 BOT_TOKEN = environ.get("BOT_TOKEN", "5857950008:AAHFOtJkAEPYCxYVMiSqCGEENNL5Yw_4oxE")
 ADMIN = int(environ.get("ADMIN", "1957296068"))          
 CAPTION = environ.get("CAPTION", "Nothing")
-
+log_chat = environ.get("LOG", "-1001684591826")
 class temp(object):
     THUMBNAIL = environ.get("THUMBNAIL", "https://telegra.ph/file/7e35a32c78758080dd9e7.jpg")
