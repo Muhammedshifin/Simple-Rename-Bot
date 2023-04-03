01ed860867461ee086a210aa6dfb3d348da6cb1f
Muhammedshifin <muhammedshifink4@gmail.com>
2023-04-03 20:03:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "18302370"))
 API_HASH = environ.get("API_HASH", "03c2cced4dea9b1e96dce87558dd2381")
-BOT_TOKEN = environ.get("BOT_TOKEN", "5857950008:AAHFOtJkAEPAAHFOtJkAAHFOtJ")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6109845361:AAH4FROLCWm__f74n9CP75GtjwCCpVuTqh4")
 ADMIN = int(environ.get("ADMIN", "1957296068"))          
 CAPTION = environ.get("CAPTION", "Nothing")
 log_chat = environ.get("LOG", "-1001684591826")
