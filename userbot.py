8480bdc2c17081b45f842fad7ab87f6a07165f2d
Muhammedshifin <muhammedshifink4@gmail.com>
2023-04-03 19:56:35 +0530

    Update userbot.py

--- userbot.py
+++ userbot.py
@@ -1,10 +1,10 @@
 from pyrogram import Client, filters, errors, enums
 from pyrogram import idle
-
+from config import *
 
 API_ID = int(environ.get('API_ID', "18302370"))
 API_HASH = environ.get('API_HASH', "03c2cced4dea9b1e96dce87558dd2381")
-SESSION = environ.get('SESSION', "")
+SESSION = environ.get('SESSION', "BQDimrUAKQC-vbs2RIbp245-hLmJUUjqnloUomagXSZ20ekgt4C9qnLNp9s99WWk8SABL7sbOdhCop4I9ab3lrIpIBagtGulYi2jQKRpDdCj0emF0ivf8VE56wgvgX1EJHJ3uP7pyAOLpc-ws63mvNp_k9s8atng2SbiLTpkM5FobBDQ4dYUQtm3zMk1lBfem1QyhxgQ-EDOoPZa0Bmibod7SKFB1OrupuxM4HLNuFjNGLyT0qboaqsvn4dB14bxvUMFK_zq2hvPTj96Sv1V2dGOLs6vGk0jYVe5fS89TFPGPb__8d5rbi_W75r6Pc0S0b6S5NA63gjsj-64dasMf_4doLNOXwAAAAFSdMRBAA")
 
 User = Client(name="user-account",
               session_string=SESSION,
