6e33ce29517f716dc77b7ef5bce5125ecdd93cb6
Muhammedshifin <muhammedshifink4@gmail.com>
2023-04-07 08:11:13 +0530

    Update userbot.py

--- userbot.py
+++ userbot.py
@@ -4,7 +4,7 @@ from config import *
 
 API_ID = int(environ.get('API_ID', "18302370"))
 API_HASH = environ.get('API_HASH', "03c2cced4dea9b1e96dce87558dd2381")
-SESSION = environ.get('SESSION', "BQDimrUAKQC-vbs2RIbp245-hLmJUUjqnloUomagXSZ20ekgt4C9qnLNp9s99WWk8SABL7sbOdhCop4I9ab3lrIpIBagtGulYi2jQKRpDdCj0emF0ivf8VE56wgvgX1EJHJ3uP7pyAOLpc-ws63mvNp_k9s8atng2SbiLTpkM5FobBDQ4dYUQtm3zMk1lBfem1QyhxgQ-EDOoPZa0Bmibod7SKFB1OrupuxM4HLNuFjNGLyT0qboaqsvn4dB14bxvUMFK_zq2hvPTj96Sv1V2dGOLs6vGk0jYVe5fS89TFPGPb__8d5rbi_W75r6Pc0S0b6S5NA63gjsj-64dasMf_4doLNOXwAAAAFSdMRBAA")
+SESSION = environ.get('SESSION', "BQCjLnL8egVI1vOcV0GE8JfJPMYSHIWWSXjl1V2d8JPhkQIXPuCis4xek4hhOaK3FBLB9d1XUkcjgzdoImdcekCLarcWkdqXnLuYlW4KKAd8YL9xYBDt3y9FQFLD5MlHKyIWUsfMOWo8TRhoovJ_SF17G-egyPtvW_1L65gcp-Uz1l3dn2Ac4cZ3dl6hX5-RZkOz_iEharA8nVyjFWy5hPPoA8RTmc4kSi1PjF701HKP2OgMWV3uBRsl-D3BD-NXzmzbcY8FlPRol1inPbN-vWy9lTM5Yg7hqtJvO3dL9wHEVhOVJMOY48XsBOtMjB7C63jo--HBlYyTWYL1AmRYJcjaAAAAAU2Yx_4A")
 
 User = Client(name="user-account",
               session_string=SESSION,
